0fca90bf45f0da36766db3fdff76bea6814843ed
apple <apple@appledeiMac.local>
2018-11-27 17:41:09 +0800

    oo

--- TestVCDemoPT/cankao/ArticalDetailController.h
+++ TestVCDemoPT/cankao/ArticalDetailController.h
@@ -6,15 +6,15 @@
 //  Copyright © 2018年 DaDaoZhiXing. All rights reserved.
 //
 
-#import "BaseViewController.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface ArticalDetailController : BaseViewController
-
-@property(nonatomic,assign)NSInteger articalId;
-
-@property(nonatomic,strong)NSString *articalTitle;
-@end
+//#import "BaseViewController.h"
+//
+//NS_ASSUME_NONNULL_BEGIN
+//
+//@interface ArticalDetailController : BaseViewController
+//
+//@property(nonatomic,assign)NSInteger articalId;
+//
+//@property(nonatomic,strong)NSString *articalTitle;
+//@end
 
 NS_ASSUME_NONNULL_END
