4b815482345d70a04069ecd401408603072e0a99
apple <apple@appledeiMac.local>
2018-11-28 18:29:53 +0800

    Ij

--- TestVCDemoPT/AppDelegate.m
+++ TestVCDemoPT/AppDelegate.m
@@ -20,10 +20,10 @@
 //    NSString *vcName = @"JYSettingViewController";
 //   NSString *vcName =  @"JYVideoTestListViewController";
 //    NSString *vcName = @"JYMsgInfoViewController";
-//    NSString *vcName = @"JYFeedbackViewController";
+    NSString *vcName = @"JYFeedbackViewController";
 //    NSString *vcName = @"JYBindPhoneViewController";
 //    NSString *vcName = @"JYBindPhoneNumViewController";
-     NSString *vcName = @"JYEditMineViewController";
+//     NSString *vcName = @"JYEditMineViewController";
      [[VCManager sharedManager] regsisterClass:vcName obj:vcName];
      Class c = [[VCManager sharedManager] getClass:vcName];
     id obj = [[c alloc] init];
